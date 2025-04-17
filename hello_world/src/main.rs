ea4965cf8429db4a75699433f28e55aad6fb748a
zkan <kan@odds.team>
2025-04-17 16:48:34 +0700

    Create new module and use it in main

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -3,11 +3,17 @@ mod my_func;
 use my_func::my_func;
 use my_func::MyStruct;
 
+mod my_utils;
+
+use my_utils::greeting::hey;
+
 fn main() {
     my_func();
 
     let my_struct = MyStruct {
         name: "John".to_string(),
     };
-    println!("{}", my_struct.name)
+    println!("{}", my_struct.name);
+
+    hey();
 }
