fb08ef43c8e36e2a2d1fbc8a4cdd3fb5a94b2896
zkan <kan@odds.team>
2025-04-17 15:57:02 +0700

    Declare use func

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,10 +1,13 @@
 mod my_func;
 
+use my_func::my_func;
 use my_func::MyStruct;
 
 fn main() {
-    my_func::my_func();
+    my_func();
 
-    let my_struct = MyStruct { name: "John".to_string() };
+    let my_struct = MyStruct {
+        name: "John".to_string(),
+    };
     println!("{}", my_struct.name)
 }
