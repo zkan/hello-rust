b59960124b8e4e1c6a9e7891543ecb9fdd4eae90
zkan <kan@odds.team>
2025-04-17 15:54:39 +0700

    Put func into separate module

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
+mod my_func;
+
+use my_func::MyStruct;
+
 fn main() {
-    println!("Hello, world!");
+    my_func::my_func();
+
+    let my_struct = MyStruct { name: "John".to_string() };
+    println!("{}", my_struct.name)
 }
---
+++ hello_world/src/my_func.rs
@@ -0,0 +1,7 @@
+pub fn my_func() {
+    println!("Hello, world!");
+}
+
+pub struct MyStruct {
+    pub name: String,
+}
